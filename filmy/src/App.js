ae1e458b8e361ca1ef5e1556936b77989bea3ad8
DESKTOP-BAUGCC9\Hempel 4Ti <hempel.maciej3@gmail.com>
2024-09-30 11:03:04 +0200

    bootstrap

--- filmy/src/App.js
+++ filmy/src/App.js
@@ -1,28 +1,22 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
-
 function App() {
   return (
     <div className="App">
       <form>
-        <div>
           <div class="mb-3">
-            <label for="film" class="form-label">Tytuł filmu: </label>
+            <div for="film" class="form-label">Tytuł filmu:</div>
             <input type="text" class="form-control" id="film" />
           </div>
-          
-        </div>
-        
-        <select class="form-select" aria-label="Default select example">
-          <option selected>Wybierz rodzaj</option>
+          <div id="film" class="form-label">Wybierz rodzaj filmu</div>
+        <select id="select" class="form-control">
           <option></option>
           <option value="1">Komedia</option>
           <option value="2">Obyczajowy</option>
           <option value="3">Sensacyjny</option>
           <option value="4">Horror</option>
         </select>
-        <div>
-        <button type="submit" class="btn btn-primary">Dodaj</button></div>.
+        <div><button type="submit" class="btn btn-primary">Dodaj</button></div>.
       </form>
     </div>
   );
